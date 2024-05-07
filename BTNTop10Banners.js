92d375c2cf74784697830fe5b1ff061f2fd4c462
gunnerkidBT <169069404+gunnerkidBT@users.noreply.github.com>
2024-05-07 11:35:03 -0500

    Update BTNTop10Banners.js

--- BTNTop10Banners.js
+++ BTNTop10Banners.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BTN Top 10 Banners
-// @version      1.1
+// @version      1.2
 // @grant        GM_xmlhttpRequest
 // @grant        GM.notification
 // @match        https://broadcasthe.net/top10.php
@@ -12,43 +12,40 @@
 (function() {
     'use strict';
 
+    // Function to add banners to all torrent columns
     function addAllBanners() {
-        const torrentColumn = document.querySelectorAll('#content tbody tr.colhead');
-        torrentColumn.forEach((el,key) => {
+        const torrentColumns = document.querySelectorAll('#content tbody tr.colhead');
+
+        torrentColumns.forEach((column) => {
             const bannerTd = document.createElement('td');
-            bannerTd.className = 'banner';
-            el.insertBefore(bannerTd, el.children[2]);
-        })
+            bannerTd.classList.add('banner');
+            column.insertBefore(bannerTd, column.children[2]);
+        });
+
         for (let i = 0; i <= 59; i++) {
             getUrlForBanner(i);
         }
     }
 
-    //Get URL for Title
+    // Function to search for TV title URL
     function searchTitleUrl(rowCount) {
-        var element = [];
-        let aElements = document.getElementsByTagName('a');
-        for (let i = 0; i < aElements.length; i++) {
-            let aElement = aElements[i];
-            if (aElement.href.includes('series.php?id=')) {
-                element.push(aElement.href);
-            }
-        }
-        return element[rowCount];
+        const aElements = document.querySelectorAll('a[href*="series.php?id="]');
+        return aElements[rowCount] ? aElements[rowCount].href : null;
     }
 
+    // Function to get URL for banner
     function getUrlForBanner(rowCount) {
-        let tvTitleUrl = searchTitleUrl(rowCount);
+        const tvTitleUrl = searchTitleUrl(rowCount);
         if (tvTitleUrl) {
             GM_xmlhttpRequest({
                 method: "GET",
                 url: tvTitleUrl,
                 onload: function(response) {
-                    let parser = new DOMParser();
-                    let doc = parser.parseFromString(response.responseText, "text/html");
-                    let spanElement = doc.querySelector("#banner");
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(response.responseText, "text/html");
+                    const spanElement = doc.querySelector("#banner");
                     if (spanElement) {
-                        let bannerUrl = spanElement.src;
+                        const bannerUrl = spanElement.src;
                         addBannerToTable(bannerUrl, rowCount);
                     }
                 }
@@ -56,17 +53,26 @@
         }
     }
 
+    // Function to add banner to the table
     function addBannerToTable(bannerUrl, rowCount) {
-        const torrentRow = document.querySelectorAll('#content tbody tr.group_torrent');
+        const torrentRows = document.querySelectorAll('#content tbody tr.group_torrent');
         const bannerTd = document.createElement('td');
-        bannerTd.className = 'banner1';
-        const bannerImg = document.createElement('img');
+        bannerTd.classList.add('banner1');
+        const bannerImg = new Image();
         bannerImg.style.width = '100%';
         bannerImg.src = bannerUrl;
         bannerTd.appendChild(bannerImg);
-        torrentRow[rowCount].insertBefore(bannerTd, torrentRow[rowCount].children[2]);
-}
+        if (torrentRows[rowCount]) {
+            torrentRows[rowCount].insertBefore(bannerTd, torrentRows[rowCount].children[2]);
+        }
+    }
 
-    addAllBanners();
+    // Initialization function
+    function init() {
+        addAllBanners();
+    }
+
+    // Call the initialization function
+    init();
 
 })();
